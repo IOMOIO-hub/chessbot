cc204bde7ca2f1272ee573fd3a17951d13461889
Vadim Karataev <karataev.vadim.a@gmail.com>
2023-09-27 18:49:06 +0500

    Test for /help command

--- chess/src/test/java/oop/chessbot/AppTest.java
+++ chess/src/test/java/oop/chessbot/AppTest.java
@@ -1,11 +1,27 @@
 package oop.chessbot;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+
+import static org.junit.Assert.assertEquals;
+
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
 
 public class AppTest {
     @Test
-    public void shouldAnswerWithTrue() {
-        assertTrue( true );
+    public void helpCommandTest() {
+        final ByteArrayOutputStream outContent = new ByteArrayOutputStream();
+        final PrintStream originalOut = System.out;
+
+        System.setOut(new PrintStream(outContent));
+
+        Bot bot = Bot.getInstance();
+        Chat chat = bot.createChat("username");
+        
+        bot.execute("/help", "username");
+
+        System.setOut(originalOut);
+        
+        assertEquals("/play - начать новую игру \n/help - список команд\r\n", outContent.toString());
     }
 }
