9828b571ef22e162aefc2ddf0a0e9eb59952b029
Geratoptus <shura.bocharov.2004@mail.ru>
2023-10-08 19:56:16 +0500

    class Queen was realised

--- chess/src/main/java/oop/chessbot/Game.java
+++ chess/src/main/java/oop/chessbot/Game.java
@@ -102,36 +102,36 @@ public class Game {
             ArrayList<Coord> result = new ArrayList<Coord>();
             Coord placement = this.getPlacement();
             int x = placement.getX(), y = placement.getY();
-            for (int i = x + 1, j = y + 1; i < 8 && j < 8; i++, j++){
-                if (board[i][j] == null){
-                    result.add(new Coord(i, j));
+            for (int tempX = x + 1, tempY = y + 1; tempX < 8 && tempY < 8; tempX++, tempY++){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
                 }
                 else{
                     break;
                 }
             }
 
-            for (int i = x + 1, j = y - 1; i < 8 && j > -1; i++, j--){
-                if (board[i][j] == null){
-                    result.add(new Coord(i, j));
+            for (int tempX = x + 1, tempY = y - 1; tempX < 8 && tempY > -1; tempX++, tempY--){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
                 }
                 else{
                     break;
                 }
             }
 
-            for (int i = x - 1, j = y - 1; i > -1 && j > -1; i--, j--){
-                if (board[i][j] == null){
-                    result.add(new Coord(i, j));
+            for (int tempX = x - 1, tempY = y - 1; tempX > -1 && tempY > -1; tempX--, tempY--){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
                 }
                 else{
                     break;
                 }
             }
 
-            for (int i = x - 1, j = y + 1; i > -1 && j < 8; i--, j++){
-                if (board[i][j] == null){
-                    result.add(new Coord(i, j));
+            for (int tempX = x - 1, tempY = y + 1; tempX > -1 && tempY < 8; tempX--, tempY++){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
                 }
                 else{
                     break;
@@ -195,7 +195,89 @@ public class Game {
     }
     
     private class Queen extends Figure{
+        public ArrayList<Coord> possibleTurns(){
+            ArrayList<Coord> result = new ArrayList<Coord>();
+            Coord placement = this.getPlacement();
+            int x = placement.getX(), y = placement.getY();
 
+            for (int tempX = x + 1; tempX < 8; tempX++){
+                if (board[tempX][y] == null){
+                    result.add(new Coord(tempX, y));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempY = y - 1; tempY > -1; tempY--){
+                if (board[x][tempY] == null){
+                    result.add(new Coord(x, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempX = x - 1; tempX > -1; tempX--){
+                if (board[tempX][y] == null){
+                    result.add(new Coord(tempX, y));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempY = y + 1; tempY < 8; tempY++){
+                if (board[x][tempY] == null){
+                    result.add(new Coord(x, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempX = x + 1, tempY = y + 1; tempX < 8 && tempY < 8; tempX++, tempY++){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempX = x + 1, tempY = y - 1; tempX < 8 && tempY > -1; tempX++, tempY--){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempX = x - 1, tempY = y - 1; tempX > -1 && tempY > -1; tempX--, tempY--){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            for (int tempX = x - 1, tempY = y + 1; tempX > -1 && tempY < 8; tempX--, tempY++){
+                if (board[tempX][tempY] == null){
+                    result.add(new Coord(tempX, tempY));
+                }
+                else{
+                    break;
+                }
+            }
+
+            return result;
+        }
+
+        public char getType(){
+            return 'q';
+        }
     }
     
     private class King extends Figure{
