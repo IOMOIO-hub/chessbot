037682fee328c4c65e4bbd86fa577c4a1ecd6445
Geratoptus <shura.bocharov.2004@mail.ru>
2023-10-08 20:02:16 +0500

    class King was realised

--- chess/src/main/java/oop/chessbot/Game.java
+++ chess/src/main/java/oop/chessbot/Game.java
@@ -281,7 +281,25 @@ public class Game {
     }
     
     private class King extends Figure{
+        public ArrayList<Coord> possibleTurns(){
+            ArrayList<Coord> result = new ArrayList<Coord>();
+            Coord placement = this.getPlacement();
+            int x = placement.getX(), y = placement.getY();
+            int[][] kingsTurns = {{1, -1}, {1, 0}, {1, 1}, {-1, -1}, {-1, 0}, {-1, 1}, {0, 1}, {0, -1}};
+            
+            for (int i = 0; i < 8; i++){
+                int newX = x + kingsTurns[i][0], newY = y + kingsTurns[i][1];
+                if ((board[newX][newY] == null) && (newX >= 0) && (newX <= 7) && (newY >= 0) && (newY <= 7)){
+                    result.add(new Coord(newX, newY));
+                }
+            }
+            
+            return result;
+        }
 
+        public char getType(){
+                return 'K';
+        }
     }
 
     private Figure selectedFigure;
